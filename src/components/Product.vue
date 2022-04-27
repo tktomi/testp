6f7606d6c8b44306eccffe0285f3658abdb7baac
tktomi <35882638+tktomi@users.noreply.github.com>
2022-04-27 17:27:41 +0900

    Initial commit

---
+++ src/components/Product.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-col cols="12" sm="6" md="4" lg="4">
+    <v-card class="mx-auto">
+      <v-img
+        class="white--text align-end"
+        height="200px"
+        :src="product.image">
+        <v-card-title class="card-title">{{ product.name }}</v-card-title>
+      </v-img>
+
+      <v-card-subtitle class="pb-0">{{ product.year }}</v-card-subtitle>
+
+      <v-card-text class="text--primary">
+        <div>{{ product.category }}</div>
+        <div>{{ product.use }}</div>
+        <div>{{ product.info }}</div>
+      </v-card-text>
+
+      <v-card-actions
+        v-for="(url, key) in product.urls"
+        :key="key">
+        <a :href="url" target="_blank">
+          <v-btn color="orange" text>
+            LINK
+          </v-btn>
+        </a>
+      </v-card-actions>
+    </v-card>
+  </v-col>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: ["product"],
+};
+</script>
+
+<style lang="scss">
+.card-title {
+  text-shadow : 3px 3px 3px #000000;
+}
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <v-row>
+      <product
+        v-for="(product, key) in filterdList"
+        :key="key"
+        :product="product"
+      >
+      </product
+    ></v-row>
+  </div>
+</template>
+
+<script>
+import Product from './Product.vue';
+export default {
+  name: 'ProductList',
+  components: { Product },
+  data: () => ({
+    sortYear: '全て',
+    products: [
+      {
+        id: 1,
+        name: 'ここにタイトルを入れてね',
+        // TODO: ここに差し替えたい画像のパスを入れてね
+        image: require('@/assets/re-vr.png'),
+        year: '2020',
+        category: 'プロジェクト',
+        use: 'ここに使った技術を入れてね',
+        urls: [
+          // TODO: ここにリンクしたいURLを入れてね
+          'http://google.com'
+        ],
+      },
+      // image: "vue-portfolio/src/assets/re-vr.png",
+      // {
+      //   id: 2,
+      //   name: "work2",
+      //   image: "https://dummyimage.com/250x150/000/fff",
+      //   year: "2019",
+      //   category: "LP",
+      //   use: "Vue.js",
+      // },
+      // {
+      //   id: 3,
+      //   name: "work3",
+      //   image: "https://dummyimage.com/250x150/000/fff",
+      //   year: "2018",
+      //   category: "ECサイト",
+      //   use: "デザイン",
+      // },
+      // {
+      //   id: 4,
+      //   name: "work4",
+      //   image: "https://dummyimage.com/250x150/000/fff",
+      //   year: "2019",
+      //   category: "LP",
+      //   use: "Vue.js",
+      // },
+    ],
+  }),
+  computed: {
+    filterdList: function() {
+      const newList = [];
+      for (let i = 0; i < this.products.length; i++) {
+        if (this.sortYear === "2020" && this.products[i].year === "2020") {
+          newList.push(this.products[i]);
+        } else if (
+          this.sortYear === "2019" &&
+          this.products[i].year === "2019"
+        ) {
+          newList.push(this.products[i]);
+        } else if (
+          this.sortYear === "2018" &&
+          this.products[i].year === "2018"
+        ) {
+          newList.push(this.products[i]);
+        } else if (this.sortYear === "全て") {
+          newList.push(this.products[i]);
+        }
+      }
+      return newList;
+    },
+  },
+  methods: {
+    sortYearChanged: function(year) {
+      console.log(year);
+      this.sortYear = year;
+    },
+  },
+};
+</script>
